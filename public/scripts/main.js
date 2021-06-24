0dffac05c81969b0c9add73a45348942a75cffc3
Thiago Tassinari <thiagotassinari12@gmail.com>
2021-06-24 16:10:35 -0300

    Requisição de abrir e fechar Modal e os nomes dos compoenentes OK

---
+++ public/scripts/main.js
@@ -0,0 +1,34 @@
+import Modal from './modal.js'
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+const modal = Modal()
+
+// Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+    button.addEventListener("click", handleClick)
+})
+
+/* Quando o botão delete for clicado ele abra a modal */
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+    
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    
+    modal.open()
+}
+
